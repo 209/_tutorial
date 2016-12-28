5d80a7bd419c5a62a1907a479d86357f0e312e7f
209 <kolevatykh.ilja@gmail.com>
2016-12-28 08:14:33 +0300

    add refactoring structure

--- app/static/app.js
+++ app/static/app.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 class Main {
     constructor() {
-
+        //здесь будет запуск всего
     }
 }
 
--- app/static/init.js
+++ app/static/init.js
@@ -1,6 +1,6 @@
 
 import _ from 'lodash';
-import App from 'app';
+import App from 'App';
 
 class Init {
     constructor() {
