2bbcd536eb95e07c58c7f9165882a9e3daea2d50
209 <kolevatykh.ilja@gmail.com>
2017-01-09 23:47:28 +0300

    add updating webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,26 @@
 var path = require('path');
 
 module.exports = {
-    context: __dirname + '',
+    context: path.join(__dirname),
     resolve: {
-        root: __dirname + '/app/static'
+        root: path.join(__dirname, '/app/static')
     },
     entry: {
-        init: __dirname + '/app/static/init.js'
+        init: path.join(__dirname, '/app/static/init.js')
     },
     output: {
-        path: __dirname + '/app/build',
+        path: path.join(__dirname, '/app/build'),
+
         // libraryTarget: 'amd',
         filename: 'init.js'
     },
     devtool: '#inline-source-map',
-    module: {
+    module:  {
         loaders: [{
-            test: /\.js$/,
+            test:    /\.js$/,
             exclude: /node_modules/,
-            loader: 'babel',
-            query: {
+            loader:  'babel',
+            query:   {
                 presets: ['es2015'],
                 plugins: [
                     'transform-class-properties',
